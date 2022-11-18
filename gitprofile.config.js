5f95fc17b3b0364e066a6251c2ecff169d7c0b2a
Dave Farnham <dave.farnham@icloud.com>
2022-11-18 13:28:15 -0700

    updated

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: ['rustc-dev-guide','dfarnham.github.io'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['rustc-dev-guide','dfarnham.github.io','Notebooks','anagram'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
